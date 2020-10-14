e1fb581970a27dc4d3087db53b8b750ff99fbedc
Arix2019 <arix0001@gmail.com>
2020-10-14 09:50:30 -0300

    adicionado os.totalmem()

--- sys_info.js
+++ sys_info.js
@@ -2,9 +2,10 @@
 const os = require('os');
 
 console.log("Nome do host: " + os.hostname() +
-            "\nSistema Operacional: " + os.type() + 
-	    "\nDiretório atual do usuário: " + os.homedir() + 
+            "\nSistema Operacional: " + os.type() +
+	    "\nDiretório atual do usuário: " + os.homedir() +
 	    "\nRelease: " + os.release() +
-	    "\nArquitetura da CPU: " + os.arch() +	
+	    "\nArquitetura da CPU: " + os.arch() +
+	    "\nMemória total do sitema: " + os.totalmem() +
             "\nMemória disponivel no sitema: " + os.freemem())
 
